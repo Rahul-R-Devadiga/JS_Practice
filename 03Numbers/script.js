dc920da85ce0a90840cfd958ee9f033daaad85b5
rahul <rdev05.rd@gmail.com>
2024-04-14 22:07:07 +0530

    added all neccessary files to understand numbers and it's arithmatic operations through simple calculator program

---
+++ 03Numbers/script.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("add").addEventListener("click", function () {
+        calculate("add");
+    });
+
+    document.getElementById("sub").addEventListener("click", function () {
+        calculate("sub");
+    })
+    document.getElementById("multiply").addEventListener("click", function () {
+        calculate("multiply");
+    })
+    document.getElementById("divide").addEventListener("click", function () {
+        calculate("divide");
+    })
+    document.getElementById("modulus").addEventListener("click", function () {
+        calculate("modulus");
+    })
+
+    function calculate(operations) {
+
+        let num1 = parseFloat(document.getElementById("num1").value);
+        let num2 = parseFloat(document.getElementById("num2").value);
+        switch (operations) {
+            case "add":
+                document.getElementById("result").innerHTML = num1 + num2;
+                break;
+            case "sub":
+                document.getElementById("result").innerHTML = num1 - num2;
+                break;
+
+            case "multiply":
+                document.getElementById("result").innerHTML = num1 * num2;
+                break;
+
+            case "divide":
+                if(num2 === 0){
+                    document.getElementById("result").innerHTML = "Can't be divided by 0"
+                }else{
+                    document.getElementById("result").innerHTML = num1/num2;
+                }
+                break;
+
+            case "modulus":
+                if(num2 === 0){
+                    document.getElementById("result").innerHTML = "Can't be divided by 0"
+                }else{
+                    document.getElementById("result").innerHTML = num1 % num2;
+                }
+                break;
+
+            default:
+                document.getElementById("result").innerHTML = "Invalid Choice";
+                break;
+        }
+    }
+});
